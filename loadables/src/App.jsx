d05a0772928ba1c3b1917b5325245bc8568c815d
tanishka <tsdineshjai@gmail.com>
2024-03-30 16:32:59 +0530

    useRecoilValueLoadable & useRecoilStateLoadable intro

--- loadables/src/App.jsx
+++ loadables/src/App.jsx
@@ -1,19 +1,49 @@
+/* eslint-disable react/prop-types */
 import "./App.css";
-import { RecoilRoot, useRecoilValue } from "recoil";
+import { RecoilRoot, useRecoilValue, useRecoilValueLoadable } from "recoil";
+import { getDataAtomFamily } from "./atoms";
 
 function App() {
 	return (
-		<>
-			<RecoilRoot>
-				<Todo />
-				<h3>App is up and running</h3>
-			</RecoilRoot>
-		</>
+		<RecoilRoot>
+			<TodoItem todoITemID={1} />
+			<TodoItem todoITemID={2} />
+		</RecoilRoot>
 	);
 }
 
-function Todo() {
-	return <></>;
-}
-
 export default App;
+
+function TodoItem({ todoITemID }) {
+	const todoItemLoadable = useRecoilValueLoadable(
+		getDataAtomFamily(todoITemID)
+	);
+
+	const { id, title, description } = todoItemLoadable.contents;
+
+	switch (todoItemLoadable.state) {
+		case "hasValue":
+			return (
+				<>
+					<div
+						key={id}
+						style={{
+							border: "1px solid white",
+							borderRadius: "11px",
+							marginBottom: "0.75rem",
+							padding: "0.55rem",
+						}}
+					>
+						<p>{title}</p>
+						<p>{description}</p>
+					</div>
+				</>
+			);
+
+		case "loading":
+			return <h1>Loading....</h1>;
+
+		case "hasError":
+			throw todoItemLoadable.contents;
+	}
+}
